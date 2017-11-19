080cedc2db933a9195476dd34a4bcbcfaac2ad57
hiroki_shimada <hsnonsense5@gmail.com>
2017-11-19 15:37:17 +0900

    provider修正

--- provider.tf
+++ provider.tf
@@ -4,6 +4,6 @@ variable "pagerduty_token" {}
  * プロバイダー設定
  * https://www.terraform.io/docs/providers/pagerduty/index.html
  */
-provider "google" {
+provider "pagerduty" {
   token = "${var.pagerduty_token}"
 }
